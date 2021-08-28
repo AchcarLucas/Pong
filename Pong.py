8981dd623df8e8c5184ff3cd3e960805f1ea2893
Lucas Campos <achcarlucas@hotmail.com>
2021-08-27 23:09:09 -0300

    atualização do código pong

--- Pong.py
+++ Pong.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+import math
 
 # define o tamanho da tela
 screenSize = (800, 600)
@@ -45,7 +47,7 @@ variablesMaquina = GPlayer()
 ##########################
 
 # velocidade do jogador (1.0 / 60.0 é igual a 1 pixel por segundo)
-variablesJogador.velocidade_inicial = 60.0 / FPS
+variablesJogador.velocidade_inicial = 30.0 / FPS
 variablesJogador.velocidade = variablesJogador.velocidade_inicial 
 
 # altura do jogador
@@ -67,7 +69,7 @@ variablesJogador.y = variablesJogador.y_inicial
 ##########################
 
 # velocidade da máquina (1.0 / 60.0 é igual a 1 pixel por segundo)
-variablesMaquina.velocidade_inicial = 60.0 / FPS
+variablesMaquina.velocidade_inicial = 40.0 / FPS
 variablesMaquina.velocidade = variablesMaquina.velocidade_inicial 
 
 # altura da máquina
@@ -90,8 +92,8 @@ class GBall:
     velocidade_x = 0
     velocidade_y = 0
 
-    # direção do movimento (direita = 1 ou esquerda = -1)
-    direcao = 0
+    # direção da bola
+    dir = 0
 
     # raio da bola
     raio = 0
@@ -108,13 +110,9 @@ class GBall:
 #          Bola          #
 ##########################
 
-variablesBola = GBall()
-# define a velocidade inicial da bola
-variablesBola.velocidade_x = 60.0 / FPS
-variablesBola.velocidade_y = 60.0 / FPS
+randomAngle = math.pi / 4
 
-# direção
-variablesBola.direcao = 1
+variablesBola = GBall()
 
 # raio da bola
 variablesBola.raio = 8
@@ -123,8 +121,47 @@ variablesBola.raio = 8
 variablesBola.x_inicial = screenSize[0] / 2.0
 variablesBola.y_inicial = screenSize[1] / 2.0
 
-variablesBola.x = variablesBola.x_inicial
-variablesBola.y = variablesBola.y_inicial
+def resetBall():
+    # coloca a bola no centro da tela novamente
+    variablesBola.x = variablesBola.x_inicial
+    variablesBola.y = variablesBola.y_inicial
+
+    # randomiza o cone de lançamento
+    angleRand = (math.pi / 2) + random.uniform(-randomAngle, randomAngle)
+
+    # inicializa uma direção
+    randDir = 1
+
+    # randomiza a direção (se entrar no if, inverte a direção)
+    if(random.uniform(0, 1) >= 0.5):
+        randDir = -1
+
+    # salva a direção na bola
+    variablesBola.dir = randDir
+
+    # randomiza a velocidade inicial (0.3 até 0.8)
+    randVelocity = random.uniform(0.3, 0.8)
+
+    # inicia o cone de lançamento da bola
+    variablesBola.velocidade_x = randDir * randVelocity *  math.sin(angleRand)
+    variablesBola.velocidade_y = randVelocity * math.cos(angleRand)
+
+def revertBall():
+    # reverte a direção da bola
+    variablesBola.dir = (-1.0) * variablesBola.dir
+
+    # randomiza uma velocidade inicial
+    randVelocity = random.uniform(0.3, 0.8)
+
+    # randomiza o ângulo de lançamento
+    angleRand = (math.pi / 2) + random.uniform(-randomAngle, randomAngle)
+
+    # inicia a nova velocidade
+    variablesBola.velocidade_x = variablesBola.dir * randVelocity *  math.sin(angleRand)
+    variablesBola.velocidade_y = randVelocity * math.cos(angleRand)
+
+# faz um reset nas configurações bola antes de iniciar o jogo
+resetBall()
 
 def gameMain():
     # cria uma variavel que verifica se o jogo ainda está rodando
@@ -173,11 +210,85 @@ def update(deltaTime):
     if(key[pygame.K_s] or key[pygame.K_DOWN]):
         variablesJogador.y = variablesJogador.y + variablesJogador.velocidade * deltaTime
 
+    '''
+       movimentação da máquina (segue a bola sempre)
+       a dificuldade da máquina é definido pela altura da base e sua velocidade
+    '''
+
+    # cria uma variabel temporária que determina o centro em y da barra
+    tmp_center = variablesMaquina.y + (variablesMaquina.altura / 2.0)
+    # cria uma variavel temporária da velocidade
+    tmp_velocity = variablesMaquina.velocidade
+
+    # verifica se o centro da barra é maior que a posição y da bola (se for, movimenta a máquina para baixo)
+    if(tmp_center >= variablesBola.y):
+        # se a diferença a bola até o centro for do tamanho da bola, diminua a velocidade proporcionalmente
+        if(math.fabs(variablesBola.y - tmp_center) < variablesMaquina.altura):
+            tmp_velocity = tmp_velocity*math.fabs(variablesBola.y - tmp_center) / variablesMaquina.altura
+        # movimenta a barra
+        variablesMaquina.y = variablesMaquina.y - tmp_velocity * deltaTime
+    # aqui acontece o contrário, se a posição do centro for menor que y da bola, movimenta para cima
+    elif(tmp_center <= variablesBola.y):
+        # se a diferença a bola até o centro for do tamanho da bola, diminua a velocidade proporcionalmente
+        if(math.fabs(variablesBola.y - tmp_center) < variablesMaquina.altura):
+            tmp_velocity = tmp_velocity*math.fabs(variablesBola.y - tmp_center) / variablesMaquina.altura
+        # movimenta a barra
+        variablesMaquina.y = variablesMaquina.y + tmp_velocity * deltaTime
+
+    '''
+        Essa parte do código faz a movimentação da bola e detecção de colisão
+        da bola com o jogador e a máquina
+    '''
+
+    # movimenta a bola em x e y
+    variablesBola.x = variablesBola.x + variablesBola.velocidade_x * deltaTime
+    variablesBola.y = variablesBola.y - variablesBola.velocidade_y * deltaTime
+
+    # cria rect do jogador, máquina e da bola para utilizar com a função colliderect do módulo Rect
+    rectJogador = pygame.Rect((variablesJogador.x, variablesJogador.y, variablesJogador.largura, variablesJogador.altura))
+    rectMaquina = pygame.Rect((variablesMaquina.x, variablesMaquina.y, variablesMaquina.largura, variablesMaquina.altura))
+    rectBola    = pygame.Rect((variablesBola.x, variablesBola.y, variablesBola.raio, variablesBola.raio))
+
+    # verifica se a bola colidiu com o jogador ou máquina, se sim, modifica a direção da bola com novos parametros
+    # a verificação da dir é necessário para não ocorrer mais de uma detecção de colisão
+    if(pygame.Rect.colliderect(rectJogador, rectBola) and variablesBola.dir == -1):
+        # revert o lance com um novo cone de lançamento
+        revertBall()
+    elif(pygame.Rect.colliderect(rectMaquina, rectBola) and variablesBola.dir == 1):
+        # revert o lance com um novo cone de lançamento
+        revertBall()
+    '''
+        Essa parte do código detecta se ocorreu uma bola fora
+        se sim, reseta a bola da os pontos ao jogador e inicia
+        uma nova jogada
+    '''
+
+    # verifica se ocorreu alguma colisão nas laterais esquerda ou direita
+    if(variablesBola.x >= screenSize[0] + 2 * variablesBola.raio):
+        # reseta a bola
+        resetBall()
+        # adiciona um ponto ao jogador
+        variablesJogador.score = variablesJogador.score + 1
+    elif(variablesBola.x <= (-1) * 2 * variablesBola.raio):
+        # reseta a bola
+        resetBall()
+        # adiciona um ponto a máquina
+        variablesMaquina.score = variablesMaquina.score + 1
+
     """ 
-        detectar a colisão com as bordas da tela, só é necessário
-        a verificação no eixo y já que não há movimentação do jogador
-        e da máquina no eixo x
+        detectar a colisão com as bordas da tela do jogador máquina e da bola.
+
+        para a máquina e o jogador, só é necessário a verificação no eixo y já que não há movimentação do jogador
+        e da máquina no eixo x.
+
+        para a bola, só é necessário verificar a colisão na parte inferior e superior e reverter a velocidade no
+        eixo y
+
     """
+
+     # verifica as colisões das bordas superior e inferior (reverte a velocidade y se ocorrer colisão)
+    if(variablesBola.y >= screenSize[1] - 2 * variablesBola.raio or variablesBola.y <= 0):
+        variablesBola.velocidade_y = (-1)*variablesBola.velocidade_y
 
     # verifica a colisão nas bordas da tela para o jogador
     if(variablesJogador.y <= 0):
